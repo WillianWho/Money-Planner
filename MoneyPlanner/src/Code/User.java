3aec6732254594024eb038eb6636929fa33946f8
WillianWho <willianoliveira608@gmail.com>
2019-01-16 23:23:36 -0200

    Criação do método verificarPrivilegios, em User e novaConta em Cadastrar

--- MoneyPlanner/src/Code/User.java
+++ MoneyPlanner/src/Code/User.java
@@ -35,31 +35,51 @@ public class User implements Usuario {
         Select select = new Select();
         us.setUsername(user.getText());
         String usuario = null;
-        for(BeanUsuario u: select.select("root", "root", us)){
-           usuario = u.getUsername();
-       }
-        if(us.getUsername().equals(usuario)){
-           return v.correctField(user);
-        }else{
+        for (BeanUsuario u : select.select("root", "root", us)) {
+            usuario = u.getUsername();
+        }
+        if (us.getUsername().equals(usuario)) {
+            return v.correctField(user);
+        } else {
             return v.wrongField(user);
         }
     }
 
     @Override
-    public boolean verificarSenha(JTextField user,JPasswordField password) throws ClassNotFoundException, SQLException {
+    public boolean verificarSenha(JTextField user, JPasswordField password) throws ClassNotFoundException, SQLException {
         BeanUsuario us = new BeanUsuario();
         Select select = new Select();
-         Useful v = new Useful();
-        
+        Useful v = new Useful();
+
         String s = null;
-       for(BeanUsuario u: select.select("root", "root",user.getText(), password.getText())){
-           s = u.getPassword();
-    }
-       if(password.getText().equals(s)){
-           return v.correctField(password);
-        }else{
+        for (BeanUsuario u : select.select("root", "root", user.getText(), password.getText())) {
+            s = u.getPassword();
+        }
+        if (password.getText().equals(s)) {
+            return v.correctField(password);
+        } else {
             return v.wrongField(password);
         }
-    
-}
+
+    }
+
+    /**
+     *
+     * @param usuario
+     * @param senha
+     * @return retorna guest se for usuário convidado, root se for adm, null se der erro
+     * @throws ClassNotFoundException
+     * @throws SQLException
+     */
+    @Override
+    public String verificarPrivilegios(String usuario, String senha) throws ClassNotFoundException, SQLException {
+        
+        Select select = new Select();
+        
+        String p= "a";
+        for(BeanUsuario u: select.select(usuario, senha)){
+            p= u.getNivelDePrivilegio();
+        }
+        return p;
+    }
 }
--- moneyplanner/src/Code/Cadastrar.java
+++ moneyplanner/src/Code/Cadastrar.java
@@ -5,6 +5,7 @@
  */
 package Code;
 
+import Bean.BeanConta;
 import Bean.BeanUsuario;
 import DAO.Dao;
 import java.sql.SQLException;
@@ -16,13 +17,25 @@ import javax.swing.JTextField;
  * @author willi
  */
 public class Cadastrar {
-    public int newUserRoot(JTextField fullName,JTextField username,JPasswordField password) throws ClassNotFoundException, SQLException{
-    BeanUsuario u = new BeanUsuario();
-    Dao dao = new Dao();
-    u.setNomeCompleto(fullName.getText());
-    u.setUsername(username.getText());
-    u.setPassword(password.getText());
-    u.setNivelDePrivilegio("root");
-    return dao.insert("root", "root", u);
+
+    public int newUserRoot(JTextField fullName, JTextField username, JPasswordField password) throws ClassNotFoundException, SQLException {
+        BeanUsuario u = new BeanUsuario();
+        Dao dao = new Dao();
+        u.setNomeCompleto(fullName.getText());
+        u.setUsername(username.getText());
+        u.setPassword(password.getText());
+        u.setNivelDePrivilegio("root");
+        return dao.insert("root", "root", u);
+    }
+
+    public int novaConta(String valor, String tipo, String vencimento, String status) throws ClassNotFoundException, SQLException {
+        BeanConta nc = new BeanConta();
+        Dao dao = new Dao();
+
+        nc.setValor(Double.parseDouble(status));
+        nc.setTipo(tipo);
+        nc.setVencimento(vencimento);
+        nc.setStatus(status);
+        return dao.insert("root", "root", nc);
     }
 }
